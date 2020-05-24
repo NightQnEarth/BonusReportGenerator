7e7ff2962d24e83ef6621b2ff758170ce09f0a39
Denis Chernushchenko <NightQnEarth@gmail.com>
2020-05-24 17:29:41 +0500

    Implemented GeneratorsManager.

---
+++ BonusReportGenerator/GeneratorsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BonusReportGenerator.Bonus;
+using BonusReportGenerator.ReportGenerators;
+using BonusReportGenerator.TableParsers;
+
+namespace BonusReportGenerator
+{
+    public class GeneratorsManager
+    {
+        private readonly Dictionary<GeneratorType, ReportGenerator> reportGenerators;
+
+        public GeneratorsManager(IReportGeneratorOptions generatorOptions)
+        {
+            var contracts = CsvParser<Contract>.Parse(generatorOptions.ContractsFilepath, ContractParser.Parse)
+                .ToArray();
+            var employees = CsvParser<Employee>.Parse(generatorOptions.EmployeesFilepath, EmployeeParser.Parse)
+                .ToArray();
+            var bonuses = BonusGenerator.Generate(
+                    employees, contracts, generatorOptions.StartDateOfReport, generatorOptions.FinalDateOfReport)
+                .ToArray();
+
+            reportGenerators = new Dictionary<GeneratorType, ReportGenerator>
+            {
+                [GeneratorType.Console] = new ConsoleReportGenerator(employees, bonuses),
+                [GeneratorType.Csv] = new CsvReportGenerator(employees, bonuses, generatorOptions.OutputCsvFilepath)
+            };
+        }
+
+        public void Generate(GeneratorType generatorType) =>
+            reportGenerators[generatorType].Generate();
+    }
+}
--- BonusReportGenerator/Program.cs
+++ BonusReportGenerator/Program.cs
@@ -1,44 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using BonusReportGenerator.Bonus;
 using BonusReportGenerator.CmdClient;
 using BonusReportGenerator.ReportGenerators;
-using BonusReportGenerator.TableParsers;
 
 namespace BonusReportGenerator
 {
     public static class Program
     {
-        private static readonly Dictionary<GeneratorType, ReportGenerator> reportGenerators =
-            new Dictionary<GeneratorType, ReportGenerator>();
-
         private static void Main(string[] args)
         {
             try
             {
                 var generatorOptions = CommandLineClient.GetOptions(args);
+                var generatorsManager = new GeneratorsManager(generatorOptions);
 
-                var employees = CsvParser<Employee>.Parse(generatorOptions.EmployeesFilepath, EmployeeParser.Parse)
-                    .ToArray();
-                var contracts = CsvParser<Contract>.Parse(generatorOptions.ContractsFilepath, ContractParser.Parse)
-                    .ToArray();
-
-                var bonuses = BonusGenerator.Generate(employees,
-                                                      contracts,
-                                                      generatorOptions.StartDateOfReport,
-                                                      generatorOptions.FinalDateOfReport)
-                    .ToArray();
-
-                reportGenerators.Add(GeneratorType.Console, new ConsoleReportGenerator(employees, bonuses));
-                reportGenerators.Add(GeneratorType.Csv, new CsvReportGenerator(
-                                         employees, bonuses, generatorOptions.RedirectReportPrintingToCsv));
-
-                var generatorType = generatorOptions.RedirectReportPrintingToCsv is null
+                var generatorType = generatorOptions.OutputCsvFilepath is null
                                         ? GeneratorType.Console
                                         : GeneratorType.Csv;
 
-                reportGenerators[generatorType].Generate();
+                generatorsManager.Generate(generatorType);
             }
             catch (ArgumentParserException exception)
             {
